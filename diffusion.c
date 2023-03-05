0c33e3d8859e85615944446d04c7c508d2ad0ad1
finndillow <94394140+finndillow@users.noreply.github.com>
2023-03-05 13:08:55 -0500

    Update diffusion.c

--- diffusion.c
+++ diffusion.c
@@ -10,10 +10,41 @@
 //record the number of particles that stop at this final location
 //Feel free to declare, implement and use other functions when needed
 
+
+int ind(int x, int y, int z, int n) {
+    return (x + n) * (2 * n + 1) * (2 * n + 1) + (y + n) * (2 * n + 1) + (z + n);
+	// return (2 * n + 1) * (2 * n + 1) * (2 * n + 1) + x + y + z;
+}
+
 void one_particle(int *grid, int n)
 {
 
+    int x = 0;
+    int y = 0;
+    int z = 0;
 
+    for (int i = 0; i < n; i++) {
+
+        int random = rand() % 6;
+
+        if (random == 0) {
+            x++; 
+        } else if (random == 1) {
+            x--;
+        } else if (random == 2) {
+            y++;
+        } else if (random == 3) {
+            y--;
+        } else if (random == 4) {
+            z++;
+        } else if (random == 5) {
+            z--;
+        }
+    }
+	// int ind = (x + n) * (2 * n + 1) * (2 * n + 1) + (y + n) * (2 * n + 1) + (z + n);
+	int idx = ind(x, y, z, n);
+	//printf("idx: %d\n", idx);
+	grid[idx]++;
 
 
 }
@@ -24,11 +55,33 @@ void one_particle(int *grid, int n)
 //r*n from the origin (including particles exactly r*n away)
 //The distance used here is Euclidean distance
 //Note: you will not have access to math.h when submitting on Mimir
+
 double density(int *grid, int n, double r)
 {
+    int count = 0;
+    int tot = 0;
+    double rSquare = (r * n) * (r * n);
+	//printf("radius sq = %f\n", rSquare);
 
+    for (int x = -n; x <=n; x++) {
+        for (int y = -n; y <= n; y++) {
+            for (int z = -n; z <= n; z++) {
 
+                double totaldist = x * x + y * y + z * z;
+                
+                if (totaldist <= rSquare) {	
+                    count = count + grid[ind(x, y, z, n)];
+                }
+                
+                tot = tot + grid[ind(x, y, z, n)];
 
+				//printf("totaldist = %f\n", totaldist);
+				//printf("grid at %d = %d\n", ind(x, y, z, n), grid[(x, y, z, n)]);
+            }
+        }
+    }
+	//printf("total = %d, count = %d\n", total, count);
+    return (double) count / tot;
     
 }
 
@@ -49,10 +102,26 @@ void diffusion(int n, int m)
 {
 	//fill in a few line of code below
 
-	for(int i = 1; i<=m; i++) one_particle(grid, n);
+	// int grid;
+	int range = (2 * n + 1); 
+    int size = range * range * range;
+    int *grid = (int*) malloc(size * sizeof(int));
+
+
+
+	if (grid == NULL) {
+		printf("Memory alloc failed");
+		exit(1);
+	}
+
+	for(int i = 1; i<=m; i++) {
+		one_particle(grid, n);
+	}
 
 	print_result(grid, n);
 	//fill in some code below
+
+	free(grid);
 
 
 }
@@ -72,5 +141,8 @@ int main(int argc, char *argv[])
 	assert(m >= 1 && m <= 1000000);
 	srand(12345);
 	diffusion(n, m);
+
+	// free(grid);
+
 	return 0;
 }
